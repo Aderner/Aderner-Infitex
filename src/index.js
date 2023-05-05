a629b07d8079f4693e469bfeb6c68d71f2f96f92
Aderner <thesame779@gmail.com>
2023-05-05 16:22:26 +0100

    not necessery option

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import tasksRouter from "./routers/tasks.js";
 dotenv.config();
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 db.once("open", () => console.log("connected to database"));
